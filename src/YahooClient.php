5dab7245f817ddb92023fa6d6d9a21789270ea86
stccorp <jose.castro@stccorp.net>
2022-09-11 14:37:15 -0400

    work on createAuthUrl

--- src/YahooClient.php
+++ src/YahooClient.php
@@ -99,9 +99,18 @@ class YahooClient
     public function createAuthUrl()
     {
 
-        $url = YahooClient::AUTH_ENDPOINT . "?client_id=" . $this->client_id . "&scope=" . urlencode($this->scope ?? '') . "&nonce=" . $this->nonce . "&prompt=consent&&response_type=code&redirect_uri=" . ($this->redirect_url ?? '');
+        //$url = YahooClient::AUTH_ENDPOINT . "?client_id=" . $this->client_id . "&scope=" . urlencode($this->scope ?? '') . "&nonce=" . $this->nonce . "&prompt=consent&&response_type=code&redirect_uri=" . ($this->redirect_url ?? '');
 
-        return $url;
+        //return $url;
+          $parameters = [
+            'response_type' => 'code',
+            'scope' => $this->scope,
+            'nonce' => $this->nonce,
+            'prompt' => 'consent',
+            'client_id' => $this->client_id,
+            'redirect_uri' => $this->redirect_url
+        ];
+        return YahooClient::AUTH_ENDPOINT . '?' . http_build_query($parameters, '', '&');
     }
 
     public function fetchAccessTokenWithAuthCode($auth_code)
